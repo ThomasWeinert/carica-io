631d8162974bb2a1fe41304ba63af8226a429ffb
Thomas Weinert <thomas@weinert.info>
2013-03-19 19:42:08 +0100

    Tested: Carica\Io\Event\Emitter\Listener\On::_isset() Tested: Carica\Io\Event\Emitter\Listener\On::_get()

--- tests/Io/Event/Emitter/Listener/OnTest.php
+++ tests/Io/Event/Emitter/Listener/OnTest.php
@@ -17,5 +17,58 @@ namespace Carica\Io\Event\Emitter\Listener {
       $this->assertEquals('foo', $event->event);
       $this->assertSame($callback, $event->callback);
     }
+
+    /**
+     * @covers Carica\Io\Event\Emitter\Listener\On::__isset
+     * @dataProvider provideValidProperties
+     */
+    public function testPropertyIsset($property) {
+      $event = new On($this->getMock('Carica\Io\Event\Emitter'), 'foo', function() {});
+      $this->assertTrue(isset($event->$property));
+    }
+
+    /**
+     * @covers Carica\Io\Event\Emitter\Listener\On::__isset
+     */
+    public function testPropertyIssetWithInvalidPropertyExpectingFalse() {
+      $event = new On($this->getMock('Carica\Io\Event\Emitter'), 'foo', function() {});
+      $this->assertFalse(isset($event->INVALID_PROPERTY));
+    }
+
+    /**
+     * @covers Carica\Io\Event\Emitter\Listener\On::__get
+     */
+    public function testGetPropertyEmitter() {
+      $event = new On($emitter = $this->getMock('Carica\Io\Event\Emitter'), 'foo', function() {});
+      $this->assertSame($emitter, $event->emitter);
+    }
+
+    /**
+     * @covers Carica\Io\Event\Emitter\Listener\On::__get
+     */
+    public function testGetPropertyEvent() {
+      $event = new On($this->getMock('Carica\Io\Event\Emitter'), 'foo', function() {});
+      $this->assertEquals('foo', $event->event);
+    }
+
+    /**
+     * @covers Carica\Io\Event\Emitter\Listener\On::__get
+     */
+    public function testGetPropertyCallback() {
+      $event = new On($this->getMock('Carica\Io\Event\Emitter'), 'foo', $callback = function() {});
+      $this->assertSame($callback, $event->callback);
+    }
+
+    /**************************
+     * Data Provider
+     *************************/
+
+    public static function provideValidProperties() {
+      return array(
+        array('emitter'),
+        array('event'),
+        array('callback')
+      );
+    }
   }
 }
