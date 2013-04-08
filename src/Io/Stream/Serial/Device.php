123d2ce9b2729f24fe118990cc81b836f985a8c6
Thomas Weinert <thomas@weinert.info>
2013-04-08 10:32:21 +0200

    Fixed some variable names

--- src/Io/Stream/Serial/Device.php
+++ src/Io/Stream/Serial/Device.php
@@ -7,22 +7,22 @@ namespace Carica\Io\Stream {
     private $_device = 0;
     private $_command = '';
     
-    public function __construct($string) {
+    public function __construct($device) {
       if (substr(PHP_OS, 0, 3) === "WIN") {
         $pattern = '(^COM\d+:$)';
         $command = sprintf(
-            'mode %s BAUD=57600 PARITY=N data=8 stop=1 xon=off', strtolower($this->_device)
+            'mode %s BAUD=57600 PARITY=N data=8 stop=1 xon=off', strtolower($device)
         );
       } elseif (substr(PHP_OS, 0, 6) === "Darwin") {
         $pattern = '(^COM\d+:$)';
-        $command = sprintf('stty -F %s', $this->_device);
+        $command = sprintf('stty -F %s', $device);
       } elseif (substr(PHP_OS, 0, 5) === "Linux") {
         $pattern = '(^/dev/tty\w+\d+$)';
-        $prepare = sprintf('stty -F %s', $this->_device);
+        $prepare = sprintf('stty -F %s', $device);
       } else {
         throw new LogicException(sprintf('Unsupport OS: "%s".', PHP_OS));
       }
-      if (!preg_match($device, $pattern)) {
+      if (!preg_match($pattern, $device)) {
         throw new LogicException(sprintf('Invalid serial port: "%s".', $device));
       }
       $this->_device = $device;
