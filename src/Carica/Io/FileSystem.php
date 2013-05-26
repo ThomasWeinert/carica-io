c905c4035ae000dc3cef07fc4c4eb2b50a21a475
Thomas Weinert <thomas@weinert.info>
2013-05-26 22:15:41 +0200

    Tested and documented Carica\Io\FileSystem

--- src/Carica/Io/FileSystem.php
+++ src/Carica/Io/FileSystem.php
@@ -2,20 +2,35 @@
 
 namespace Carica\Io {
 
+  /**
+   * A wrapper/factory object for the file system.
+   *
+   * It provides and object interface for several file system related function
+   * and can create instances of splFileInfo and splFileObject for others.
+   *
+   * The object is used in other classes to provide a tesable access to
+   * the file system.
+   */
   class FileSystem {
 
+    /**
+     * Create an return a splFileInfo instance for the filename
+     *
+     * @param string $filename
+     * @return \SplFileInfo
+     */
     public function getInfo($filename) {
       return new \SplFileInfo($filename);
     }
 
-    public function getMimeType($filename) {
-      if (function_exists('mime_content_type')) {
-        return mime_content_type($filename);
-      } else {
-        return 'application/octet-stream';
-      }
-    }
-
+    /**
+     * Create and return a splFileObject isntance for the filename
+     *
+     * @param string $filename
+     * @param string $mode
+     * @param resource $context
+     * @return \SplFileObject
+     */
     public function getFile($filename, $mode = 'r', $context = NULL) {
       if (NULL === $context) {
         return new \SplFileObject($filename, $mode, FALSE);
@@ -24,6 +39,14 @@ namespace Carica\Io {
       }
     }
 
+    /**
+     * Open a file and return the resource
+     *
+     * @param string $filename
+     * @param string $mode
+     * @param resource $context
+     * @return resource
+     */
     public function getFileResource($filename, $mode = 'r', $context = NULL) {
       if (NULL === $context) {
         return fopen($filename, $mode, FALSE);
@@ -32,6 +55,24 @@ namespace Carica\Io {
       }
     }
 
+    /**
+     * Try to get the mimetype for an file.
+     *
+     * @param string $filename
+     * @return string
+     */
+    public function getMimeType($filename) {
+      return (function_exists('mime_content_type'))
+        ? mime_content_type($filename)
+        : 'application/octet-stream';
+    }
+
+    /**
+     * Wrapper for the realpath() function
+     *
+     * @param string $path
+     * @return string
+     */
     public function getRealPath($path) {
       return realpath($path);
     }
---
+++ tests/Carica/Io/FileSystemTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Carica\Io {
+
+  include_once(__DIR__.'/Bootstrap.php');
+
+  class FileSystemTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @covers Carica\Io\FileSystem::getInfo
+     */
+    public function testGetInfo() {
+      $fileSystem = new FileSystem();
+      $info = $fileSystem->getInfo(__FILE__);
+      $this->assertInstanceOf('splFileInfo', $info);
+      $this->assertEquals(
+        __FILE__,
+        (string)$info
+      );
+    }
+
+    /**
+     * @covers Carica\Io\FileSystem::getFile
+     */
+    public function testGetFile() {
+      $fileSystem = new FileSystem();
+      $file = $fileSystem->getFile(__FILE__);
+      $this->assertInstanceOf('splFileObject', $file);
+    }
+
+    /**
+     * @covers Carica\Io\FileSystem::getFile
+     */
+    public function testGetFileWithContext() {
+      $fileSystem = new FileSystem();
+      $file = $fileSystem->getFile(__FILE__, 'r', stream_context_create(array()));
+      $this->assertInstanceOf('splFileObject', $file);
+    }
+
+    /**
+     * @covers Carica\Io\FileSystem::getFileResource
+     */
+    public function testGetFileResource() {
+      $fileSystem = new FileSystem();
+      $this->assertInternalType('resource', $fh = $fileSystem->getFileResource(__FILE__));
+      fclose($fh);
+    }
+
+    /**
+     * @covers Carica\Io\FileSystem::getFileResource
+     */
+    public function testGetFileResourceWithContext() {
+      $fileSystem = new FileSystem();
+      $this->assertInternalType(
+        'resource',
+        $fh = $fileSystem->getFileResource(__FILE__, 'r', stream_context_create(array()))
+      );
+      fclose($fh);
+    }
+
+    /**
+     * @covers Carica\Io\FileSystem::getMimeType
+     */
+    public function testGetMimeTypeExpectingPhp() {
+      if (!function_exists('mime_content_type')) {
+        $this->markTestSkipped('Function "mime_content_type()" not available.');
+      }
+      $fileSystem = new FileSystem();
+      $this->assertEquals(
+        'text/x-php', $fileSystem->getMimeType(__FILE__)
+      );
+    }
+
+    /**
+     * @covers Carica\Io\FileSystem::getRealPath
+     */
+    public function testGetRealPath() {
+      $fileSystem = new FileSystem();
+      $this->assertEquals(
+        __FILE__,
+        $fileSystem->getRealPath(__DIR__.'/././FileSystemTest.php')
+      );
+    }
+  }
+}
