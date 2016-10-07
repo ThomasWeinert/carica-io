feb7aa35141443859fc8da37208ce3f9ac989858
Thomas Weinert <thomas@weinert.info>
2016-10-07 11:49:30 +0200

    Remove device address from interface (should be an object property)

--- src/Device/I2C.php
+++ src/Device/I2C.php
@@ -2,42 +2,41 @@
 
 namespace Carica\Io\Device {
 
+  /**
+   * Interface I2C Device
+   *
+   * This represents an i2c device, the address needs to be stored by the object. It should be
+   * one of the constructor arguments.
+   */
   interface I2C
   {
 
     /**
      * Write data to the i2c device
-     *
-     * @param int $slaveAddress
+     * @param array $data
      */
-    function write($slaveAddress, $data);
+    function write(array $data);
 
     /**
      * Read data from the i2c device, this is an asynchronous call,
      * the callback is execute with the received data bytes.
      *
-     * @param int $slaveAddress
      * @param int $length
      * @return \Carica\Io\Deferred\Promise
      */
-    function read($slaveAddress, $length);
-
-
+    function read($length);
 
     /**
      * Start continuous reading, repeatingly calls the listener
      *
-     * @param int $slaveAddress
      * @param int $length
      * @param callable $listener
      */
-    function startReading($slaveAddress, $length, callable $listener);
+    function startReading($length, callable $listener);
 
     /**
      * Stop continuous reading on the specified address
-     *
-     * @param int $slaveAddress
      */
-    function stopReading($slaveAddress);
+    function stopReading();
   }
 }
