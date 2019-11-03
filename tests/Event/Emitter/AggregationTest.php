b7655228f6097f0e63a7e16404a8c51d68af557c
Thomas Weinert <thomas@weinert.info>
2019-11-03 15:19:54 +0100

    [TEST] Add Tests For Event Emitter Aggregation

--- tests/Event/Emitter/AggregationTest.php
+++ tests/Event/Emitter/AggregationTest.php
@@ -3,10 +3,13 @@
 namespace Carica\Io\Event\Emitter {
 
   use Carica\Io\Event\Emitter;
+  use InvalidArgumentException;
+  use LogicException;
   use PHPUnit\Framework\MockObject\MockObject;
   use PHPUnit\Framework\TestCase;
   use ReflectionClass;
   use ReflectionException;
+  use UnexpectedValueException;
 
   include_once(__DIR__.'/../../Bootstrap.php');
 
@@ -69,6 +72,56 @@ namespace Carica\Io\Event\Emitter {
       $aggregation->events()->emit('test');
       $this->assertTrue($result);
     }
+
+    public function testAttachInvalidEventExpectingException(): void {
+      $aggregation = new Aggregation_TestProxy();
+      $this->expectException(InvalidArgumentException::class);
+      $this->expectExceptionMessage('No callable for event provided.');
+      $aggregation->onTest(NULL);
+    }
+
+    public function testAttachInvalidMethodExpectingException(): void {
+      $aggregation = new Aggregation_TestProxy();
+      $this->expectException(LogicException::class);
+      $this->expectExceptionMessage('Unknown method call');
+      $aggregation->__call('invalid', [static function() {}]);
+    }
+
+    public function testAttachInvalidMethodSilentExpectingFalse(): void {
+      $aggregation = new Aggregation_TestProxyWithCall();
+      $this->assertFalse(
+        $aggregation->callEmitterSilent('invalid', [static function() {}])
+      );
+    }
+
+    public function testAttachFailureExpectingException(): void {
+      $events = $this->createMock(Emitter::class);
+      $events
+        ->expects($this->once())
+        ->method('on')
+        ->withAnyParameters()
+        ->willThrowException(new UnexpectedValueException());
+
+      $aggregation = new Aggregation_TestProxyWithCall();
+      $aggregation->events($events);
+      $this->expectException(UnexpectedValueException::class);
+      $aggregation->__call('onTest', [static function() {}]);
+    }
+
+    public function testAttachFailureSilentExpectingFalse(): void {
+      $events = $this->createMock(Emitter::class);
+      $events
+        ->expects($this->once())
+        ->method('on')
+        ->withAnyParameters()
+        ->willThrowException(new UnexpectedValueException());
+
+      $aggregation = new Aggregation_TestProxyWithCall();
+      $aggregation->events($events);
+      $this->assertFalse(
+        $aggregation->callEmitterSilent('onTest', [static function() {}])
+      );
+    }
   }
 
   /**
@@ -86,6 +139,10 @@ namespace Carica\Io\Event\Emitter {
 
     public function __call($method, $arguments) {
       return $this->callEmitter($method, $arguments);
+    }
+
+    public function callEmitterSilent($method, $arguments): bool {
+      return $this->callEmitter($method, $arguments, TRUE);
     }
   }
 }
